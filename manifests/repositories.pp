0dbb93dac93bb3d8cb7389fb76895a05f08a2c8c
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-05-23 16:07:49 +0900

    fixed repository

--- manifests/repositories.pp
+++ manifests/repositories.pp
@@ -65,16 +65,19 @@ class arc_ce::repositories (
     }
   }
   if $enable_lcmaps or $enable_lcas {
+    package { 'epel-release':
+      ensure => installed,
+      provider => rpm,
+    }
     if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '7' {
       $source = 'http://repository.egi.eu/sw/production/umd/4/centos7/x86_64/updates/umd-release-4.1.3-1.el7.centos.noarch.rpm'
     }else{
-      $source = 'http://repository.egi.eu/sw/production/umd/3/sl6/x86_64/updates/umd-release-3.14.4-1.el6.noarch.rpm'
+      $source = 'http://repository.egi.eu/sw/production/umd/4/sl6/x86_64/updates/umd-release-4.1.3-1.el6.noarch.rpm'
     }
     package { 'umd-release':
       ensure => installed,
       source => "$source",
-      provider => rpm,
-      #require  => Package['epel-release'],
+      require  => Package['epel-release'],
     }
   }
 }
